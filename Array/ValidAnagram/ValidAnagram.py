29299626c1abf6415023e08f71ab64fa3ea9285a
Shubham-exists <shubham276533@gmail.com>
2025-07-08 23:41:16 +0530

    Create ValidAnagram.py

---
+++ Array/ValidAnagram/ValidAnagram.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        a={}
+        if len(s) != len(t):
+            return False
+        
+        for i in s:
+            a[i] = a.get(i,0)+1
+
+        for i in t:
+            if i not in a or a[i] == 0:
+                return False
+            a[i] -=1
+        
+        return True
