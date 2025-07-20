e85e956e68285ef07a9faff0709f0ee0ba0829ee
Shubham-exists <shubham276533@gmail.com>
2025-07-20 21:50:09 +0530

    Update GroupAnagrams.py

--- Array/GroupAnagrams/GroupAnagrams.py
+++ Array/GroupAnagrams/GroupAnagrams.py
@@ -3,11 +3,11 @@ class Solution:
         g={}
       
         for i in strs:
-            k=''.join(sorted(i))
+            k=''.join(sorted(i))  """ join each sorted word from strs and make a sorted string"""
         
-            if k not in g:
+            if k not in g:    #Check if the k(shorted string) not in g ,Then create a new key:vaule pair with empty list 
                 g[k]=[]
 
-            g[k].append(i)
+            g[k].append(i)  #update the empty list with value(unsorted word).
           
         return list(g.values())
