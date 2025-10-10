a27b1a45bd60c77f2d0151af8b56c93626cc568b
Shubham-exists <shubham276533@gmail.com>
2025-10-10 18:24:49 +0530

    Create K-th Symbol in Grammar.py

---
+++ Array/K-th Symbol in Grammar/K-th Symbol in Grammar.py
@@ -0,0 +1,16 @@
+class Solution:
+    def kthGrammar(self, n: int, k: int) -> int:
+        if n == 1 and k == 1:
+            return 0
+        mid = int(pow(2,n-1)/2)
+        if k <= mid:
+            return self.kthGrammar(n-1,k)
+        else:
+            return 1 - self.kthGrammar(n-1,k-mid)
+
+
+#shortes ans
+class Solution:
+    def kthGrammar(self, n: int, k: int) -> int:
+        return bin(k - 1).count('1') % 2
+      
